f6ce8268252ff0e171961fe634247813a4a8f3ac
ylann33-code <ylann.lc33@gmail.com>
2026-05-14 16:02:20 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,29 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+    getDatabase
+}
+from "https://cache-cache-geant-2d831-default-rtdb.europe-west1.firebasedatabase.app";
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyD8rwcguwvHlm55Ecf7RIShILc4T9t8Aik",
+	
+    authDomain: "cache-cache-geant-2d831.firebaseapp.com",
+	
+    projectId: "cache-cache-geant-2d831",
+	
+    storageBucket: "cache-cache-geant-2d831.firebasestorage.app",
+	
+    messagingSenderId: "5562537953",
+	
+    appId: "1:5562537953:web:2ebb1ad4299519200224a0"
+
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+
+export { db };
