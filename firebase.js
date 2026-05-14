9e5543a5817995e68ceb981b6ca2bca166c271bf
ylann33-code <ylann.lc33@gmail.com>
2026-05-14 16:36:51 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,15 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
     getDatabase
 }
-from "https://cache-cache-geant-2d831-default-rtdb.europe-west1.firebasedatabase.app";
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
 
     apiKey: "AIzaSyD8rwcguwvHlm55Ecf7RIShILc4T9t8Aik",
 	
     authDomain: "cache-cache-geant-2d831.firebaseapp.com",
+
+	 databaseURL: "https://cache-cache-geant-2d831-default-rtdb.europe-west1.firebasedatabase.app",
 	
     projectId: "cache-cache-geant-2d831",
 	
